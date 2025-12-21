2f92aa681cf8b1ef94a97e20d4645ef34b534231
6804062616124 <s6804062616124@email.kmutnb.ac.th>
2025-12-21 20:08:35 +0700

    Add files via upload

---
+++ Lab3_6.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+	int score;
+	scanf("%d",&score);
+	
+	if(score >= 68){
+		if(score > 100){
+			printf("error score");
+		}
+		else if(score >= 85){
+			printf("A");
+		}
+		else if(score >= 75){
+			printf("B");
+		}
+		else if(score >= 68){
+			printf("C\n");
+			printf("Missing %d points to get B",75-score);
+		}
+	}else{
+		if(score < 0){
+			printf("error score");
+		}
+		else if(score >= 55){
+			printf("D");
+		}
+		else{
+			printf("F");
+		}
+	}	
+	return 0;
+}
